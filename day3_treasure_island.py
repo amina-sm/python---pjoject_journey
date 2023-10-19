401ad61cbc04af2a6fb0f71c9cbd4fcadfef68e0
amina-sm <aminamayengo18@gmail.com>
2023-10-19 11:38:19 +0300

    if condition

--- day3_treasure_island.py
+++ day3_treasure_island.py
@@ -2,16 +2,16 @@
 print("Welcome ton  Tressure island.")
 print("Your mission is to find  the Treasure")
 
-cross_road=input('Your are at a cross road. where do you want to go? Type "left" or "right" ')
-if cross_road== "left":
-    lake= input('You came to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.')
-    if lake=="wait":
-        door= input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose? "red", "blue" or "yellow" ')
-        if door=="red":
+choice1=input('Your are at a cross road. where do you want to go? Type "left" or "right" \n ').lower()
+if choice1== "left":
+    choice2= input('You came to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across.\n').lower()
+    if choice2=="wait":
+        choice3= input('You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose? "red", "blue" or "yellow" \n').lower()
+        if choice3=="red":
             print("Burned on fire.\n Game over")
-        elif door=="blue":
+        elif choice3=="blue":
             print("Eaten by beasts.\n Game Over.")
-        elif door=="yellow":
+        elif choice3=="yellow":
             print("You win")
         else:
             print("Game over.")         
