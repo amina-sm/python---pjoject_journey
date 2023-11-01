ce3265e144556584c8cc7e309668f6dcf21d6b7e
amina-sm <aminamayengo18@gmail.com>
2023-11-01 16:17:48 +0300

    caesar cipher

---
+++ caesar_cypher.py
@@ -0,0 +1,40 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+direction= input("Type 'encode' to encrypt, type 'decode' to decrypt:\n") 
+text=input("Type your message:\n").lower()
+shift=int(input("Type the shift number: "))
+
+def caesar(start_text,shift_amount, cipher_direction):
+    end_text=""
+    if cipher_direction=="Decode":
+            shift_amount *= -1
+    for letter in start_text:
+        position=alphabet.index(letter)
+        new_position=position + shift_amount   
+        end_text+= alphabet[new_position]         
+    print(f"The {cipher_direction} text is {end_text}")
+    
+caesar(start_text=text,shift_amount=shift, cipher_direction=direction)
+    
+# def encrypt(plain_text,shift_amount):
+#     cipher_text=" "
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position=position + shift_amount
+#         new_letter= alphabet[new_position]
+#         cipher_text += new_letter
+#     print(f" The encoded text is {cipher_text}")   
+
+
+# def decrypt(cipher_text,shift_amount):
+#     plain_text= ""
+#     for letter in cipher_text:
+#         position = alphabet.index(letter)
+#         new_position=position - shift_amount
+#         plain_text += alphabet[new_position]
+        
+#     print(f" The decoded text is {plain_text}")   
+
+# if direction=="encode":
+#     encrypt(plain_text=text, shift_amount=shift) 
+# else:
+#     decrypt(cipher_text=text, shift_amount=shift) 
