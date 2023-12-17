ff8a549d2d2187a39ff8e889a67779d256cc335c
amina-sm <aminamayengo18@gmail.com>
2023-12-17 14:15:43 +0300

    creating table

--- day_start.py
+++ day_start.py
@@ -1,10 +1,18 @@
-from turtle import Turtle,Screen
-timmy=Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# from turtle import Turtle,Screen
+# timmy=Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-my_screen=Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+# my_screen=Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+
+from prettytable import PrettyTable
+table= PrettyTable()
+table.add_column("Family Name", ["Fatuma","Age","Tatu"])
+table.add_column("Age", [12,8,4])
+table.align="l"
+print(table)
