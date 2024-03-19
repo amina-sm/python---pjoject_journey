a76a794ab531f438f55b8ba7107608b2c1fbde89
amina-sm <aminamayengo18@gmail.com>
2024-03-19 08:42:55 +0300

    snake game

---
+++ snake_game/day24/main.py
@@ -0,0 +1,6 @@
+# with open("C:\\Users\\Emmy\\Desktop\\ASM\\Journey1\\snake_game\\day24\\my_file.txt") as file:
+#     content = file.read()
+#     print(content)
+
+with open("new_file.txt", mode="w") as file:
+    file.write("New text.")
